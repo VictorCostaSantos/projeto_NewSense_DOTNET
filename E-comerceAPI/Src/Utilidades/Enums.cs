4579cc02d9e87841bc4d5f52e92f6bc8006e9c0d
VictorCostaSantos <victorcosta123vcs@gmail.com>
2022-08-23 11:25:19 -0300

    Atualizado documentação do Enum/utilidades

--- E-comerceAPI/Src/Utilidades/Enums.cs
+++ E-comerceAPI/Src/Utilidades/Enums.cs
@@ -2,7 +2,12 @@
 
 namespace E_comerceAPI.Src.Utilidades
 {
-    
+    /// <summary>
+    /// <para>Resumo: Classe responsavel por enumerar as possiveis condições da tipo de usuario|Condicao de usuario|Status do usuario|</para>
+    /// <para>Criado por: Generation</para>
+    /// <para>Versão: 1.0</para>
+    /// <para>Data: 23/08/2022</para>
+    /// </summary>
     [JsonConverter(typeof(JsonStringEnumConverter))]
     public enum CondicaoUsuario
     {
